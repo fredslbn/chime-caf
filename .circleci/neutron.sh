41eedfa61644fd92822570057ee3e03b640c3751
fredslbn <fh.silaban@gmail.com>
2024-01-27 23:13:07 +0700

    disable some configs

--- .circleci/neutron.sh
+++ .circleci/neutron.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
  #
  # Script For Building Android Kernel
  #
@@ -10,11 +9,8 @@ KERNEL_DIR="$(pwd)"
 
 ##----------------------------------------------------------##
 # Device Name and Model
-MODEL=Xiaomi
+MODEL=POCO
 DEVICE=chime
-
-# Kernel Version Code
-#VERSION=
 
 # Kernel Defconfig
 DEFCONFIG=${DEVICE}_defconfig
@@ -31,22 +27,18 @@ IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz
 # Verbose Build
 VERBOSE=0
 
-# Kernel Version
-#KERVER=$(make kernelversion)
-
-#COMMIT_HEAD=$(git log --oneline -1)
-
 # Date and Time
 DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
 TANGGAL=$(date +"%F%S")
 
 # Specify Final Zip Name
-ZIPNAME="SUPER.KERNEL-CHIME-(cosmic-clang)-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
+ZIPNAME="SUPER.KERNEL-CHIME-(neutron)-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
+FINAL_ZIP=${ZIPNAME}-${DEVICE}-${TANGGAL}.zip
 
 ##----------------------------------------------------------##
 # Specify compiler.
 
-COMPILER=cosmic-clang
+COMPILER=neutron
 
 ##----------------------------------------------------------##
 # Specify Linker
@@ -57,43 +49,53 @@ LINKER=ld.lld
 ##----------------------------------------------------------##
 # Clone ToolChain
 function cloneTC() {
-
-    if [ $COMPILER = "cosmic-clang" ];
-    then
-    git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang.git -b master cosmic-clang
-    PATH="${KERNEL_DIR}/cosmic-clang/bin:$PATH"
-    
-	fi
+        
+    if [ $COMPILER = "clang17-7" ];
+	then
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229b.tar.gz && mkdir clang && tar -xzf clang-r498229b.tar.gz -C clang/
+    export KERNEL_CLANG_PATH="${KERNEL_DIR}/clang"
+    export KERNEL_CLANG="clang"
+    export PATH="$KERNEL_CLANG_PATH/bin:$PATH"
+    CLANG_VERSION=$(clang --version | grep version | sed "s|clang version ||")
 	
-    # Clone AnyKernel
-    # git clone --depth=1 https://github.com/missgoin/AnyKernel3.git
-
-	}
-
+    wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz && tar -xf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
+    mv gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu gcc64
+    export KERNEL_CCOMPILE64_PATH="${KERNEL_DIR}/gcc64"
+    export KERNEL_CCOMPILE64="aarch64-linux-gnu-"
+    export PATH="$KERNEL_CCOMPILE64_PATH/bin:$PATH"
+    GCC_VERSION=$(aarch64-linux-gnu-gcc --version | grep "(GCC)" | sed 's|.*) ||')
+   
+    wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/arm-linux-gnueabi/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi.tar.xz && tar -xf gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi.tar.xz
+    mv gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi gcc32
+    export KERNEL_CCOMPILE32_PATH="${KERNEL_DIR}/gcc32"
+    export KERNEL_CCOMPILE32="arm-linux-gnueabi-"
+    export PATH="$KERNEL_CCOMPILE32_PATH/bin:$PATH"
+      
+    elif [ $COMPILER = "neutron" ];
+    then
+    mkdir Neutron
+    curl -s https://api.github.com/repos/Neutron-Toolchains/clang-build-catalogue/releases/latest \
+    | grep "browser_download_url.*tar.zst" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget --output-document=Neutron.tar.zst -qi -
+    tar -xvf Neutron.tar.zst -C Neutron/
+    
+    export KERNEL_CLANG="clang"
+    export KERNEL_CLANG_PATH="${KERNEL_DIR}/Neutron"
+    export PATH="$KERNEL_CLANG_PATH/bin:$PATH"
+   
+   fi
+	
+}
 
 ##------------------------------------------------------##
 # Export Variables
 function exports() {
 
-        if [ -d ${KERNEL_DIR}/cosmic ];
-           then
-               export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/cosmic/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')        
-        
-        elif [ -d ${KERNEL_DIR}/cosmic-clang ];
-           then
-               export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/cosmic-clang/bin/clang --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')       
-                
-        elif [ -d ${KERNEL_DIR}/aosp-clang ];
-            then
-               export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/aosp-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-        fi
-        
         # Export ARCH and SUBARCH
         export ARCH=arm64
         export SUBARCH=arm64
-        
-        # Export Local Version
-        # export LOCALVERSION="-${VERSION}"
         
         # KBUILD HOST and USER
         export KBUILD_BUILD_HOST=Pancali
@@ -102,17 +104,9 @@ function exports() {
 	    export PROCS=$(nproc --all)
 	    export DISTRO=$(source /etc/os-release && echo "${NAME}")
 	    
-	    # Server caching for speed up compile
-	    # export LC_ALL=C && export USE_CCACHE=1
-	    # ccache -M 100G
-	
 	}
         
 ##----------------------------------------------------------------##
-# Telegram Bot Integration
-##----------------------------------------------------------------##
-
-# Export Configs
 
 # Speed up build process
 MAKE="./makeparallel"
@@ -121,31 +115,24 @@ MAKE="./makeparallel"
 # Compilation
 function compile() {
 START=$(date +"%s")
-		
+			       
 	# Compile
-	make O=out ARCH=arm64 ${DEFCONFIG}	
-
-	if [ -d ${KERNEL_DIR}/cosmic-clang ];
+	make O=out ARCH=arm64 ${DEFCONFIG}
+	       
+	if [ -d ${KERNEL_DIR}/Neutron ];
 	   then
 	       make -j$(nproc --all) O=out \
 	       ARCH=arm64 \
-	       CC=clang \
+	       CC=$KERNEL_CLANG \
 	       CROSS_COMPILE=aarch64-linux-gnu- \
-	       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+	       CLANG_TRIPLE=aarch64-linux-gnu- \
 	       LD=${LINKER} \
 	       LLVM=1 \
 	       LLVM_IAS=1 \
-	       AR=llvm-ar \
-	       AS=llvm-as \
-	       NM=llvm-nm \
-	       OBJCOPY=llvm-objcopy \
-	       OBJDUMP=llvm-objdump \
-	       STRIP=llvm-strip \
-	       READELF=llvm-readelf \
-	       OBJSIZE=llvm-size \
 	       V=$VERBOSE 2>&1 | tee error.log
 	       
-	fi
+    fi
+    	
 }
 
 ##----------------------------------------------------------------##
@@ -154,6 +141,8 @@ function zipping() {
 	cp $IMAGE AnyKernel3
 	# cp $DTBO AnyKernel3
 	# find $DTB -name "*.dtb" -exec cat {} + > AnyKernel3/dtb
+	# find $MODULE -name "*.ko" -exec cat {} + > AnyKernel3/wtc2.ko
+	# find . -name '*.ko' -exec cp '{}' AnyKernel3/modules/system/lib/modules \;
 	
 	# Zipping and Push Kernel
 	cd AnyKernel3 || exit 1
@@ -167,6 +156,7 @@ function zipping() {
     
 }
 
+    
 ##----------------------------------------------------------##
 
 cloneTC
